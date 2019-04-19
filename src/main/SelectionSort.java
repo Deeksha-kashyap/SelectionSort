aebab755d1e5daa80079c2a47a3e13dd267ee0c3
deeksha-kashyap <deekshakashyap01@gmail.com>
2019-04-19 18:10:21 +0530

    take a selection sort method and array is n times and numElements is zero

--- src/main/SelectionSort.java
+++ src/main/SelectionSort.java
@@ -5,4 +5,9 @@ import java.util.Scanner;
 public class SelectionSort {
   private long[] array;
   private int numElements;
+
+  public SelectionSort(int n){
+    array=new long[n];
+    numElements=0;
+  }
 }
